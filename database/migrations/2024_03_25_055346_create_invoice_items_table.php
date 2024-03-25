46a458823420e8396c68daac0e52c28c5399805f
Des Dulianto <desdulianto@gmail.com>
2024-03-25 12:58:29 +0700

    create invoice item model

---
+++ database/migrations/2024_03_25_055346_create_invoice_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoice_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invoice_id')->references('id')->on('invoices');
+            $table->string('nama_item');
+            $table->integer('harga');
+            $table->integer('banyak');
+            $table->bigInteger('jumlah')->virtualAs('harga * banyak');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice_items');
+    }
+};
